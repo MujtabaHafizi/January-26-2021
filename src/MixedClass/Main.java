802c0b7c12c4cf6881ee4143b1fe25b12d2df0fa
MujtabaHafizi <muj.bdn@gmail.com>
2021-01-26 21:50:53 -0500

    sorry

--- src/MixedClass/Main.java
+++ src/MixedClass/Main.java
@@ -4,14 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Batch 8");
-        System.out.println("this will last a day");
-        System.out.println("Today is January is 26 2021");
-        System.out.println("John Travolta is the best ");
-
-        System.out.println("786");
-        System.out.println("786");
-        System.out.println("786");
-        System.out.println("1111");
-        System.out.println("2222");
+       
     }
 }
